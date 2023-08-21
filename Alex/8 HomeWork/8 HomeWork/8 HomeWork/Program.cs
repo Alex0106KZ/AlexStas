8fd4160f011eeeee4bc59789fb9ab840dcd09741
Alex <sasha2001-1@mail.ru>
2023-08-21 23:08:52 +0900

    test3

--- Alex/8 HomeWork/8 HomeWork/8 HomeWork/Program.cs
+++ Alex/8 HomeWork/8 HomeWork/8 HomeWork/Program.cs
@@ -8,8 +8,30 @@ namespace _8_HomeWork
 {
     internal class Program
     {
+
+        static void CreateLine(string chr, uint amount)
+        {
+            for (int i = 0; i < amount; i++) 
+            {
+                Console.Write(chr);
+            }
+        }
+
+       
+        
+
         static void Main(string[] args)
         {
+            Console.Write("Введите символ: ");
+            string chr = Console.ReadLine();
+
+            Console.Write("Введите количество символов: ");
+            uint amount = uint.Parse(Console.ReadLine());
+            
+
+            CreateLine(chr, amount);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Alex/8 HomeWork/8 HomeWork/8.1 HomeWork/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1_HomeWork
+{
+
+
+    internal class Program
+    {
+        static int FindIndex(int[] arr, int number)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i]  == number)
+                    return i;
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            //Создание случайного массива
+
+            Random random = new Random();
+            int[] MyArray = new int[random.Next(20)];
+
+            //Заполнение массива случайными числами
+
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                MyArray[i] = random.Next(100);
+            }
+
+            //Вывод массива в консоль
+
+            for (int i = 0;i < MyArray.Length; i++)
+            {
+                Console.Write(MyArray[i]+"\t");
+            }
+
+            Console.WriteLine();
+
+            //Использование функции
+
+            Console.Write("Введите искомое число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FindIndex(MyArray, number));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
