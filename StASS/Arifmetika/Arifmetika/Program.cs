c593189ab622fb169224ff1dcb0029c92895b6dd
StanislavAtangaraev <satangaraev@yandex.ru>
2023-08-22 21:41:43 +0600

    Commit delal otec

---
+++ StASS/Arifmetika/Arifmetika/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstAppbyStasyo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int result;
+            string str;
+
+            Console.WriteLine("Введите первое число:");
+            str = Console.ReadLine();
+            a = int.Parse(str);
+            Console.WriteLine("Введите второе число:");
+            str = Console.ReadLine();
+            b = int.Parse(str);
+
+            result = a + b;
+            Console.WriteLine("Среднее ариф. = " + result);
+
+        }
+    }
+}
---
+++ StASS/Summa3/Summa3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Summa3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first, second, third;
+            int a, b, c, summaresult, umnozhenieresult;
+
+            Console.WriteLine("Введите 3 числа:");
+            first = Console.ReadLine();
+            second = Console.ReadLine();
+            third = Console.ReadLine();
+
+            a = int.Parse(first);
+            b = int.Parse(second);
+            c = int.Parse(third);
+
+            summaresult = a + b + c;
+            umnozhenieresult = a * b * c;  
+            Console.WriteLine("Сумма 3 твоих чисел равна = " + summaresult + " Умножение равно ="+ umnozhenieresult);
+        }
+    }
+}
