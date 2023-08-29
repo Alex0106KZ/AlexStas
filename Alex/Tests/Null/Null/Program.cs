2417e93159dd50815c94dffd30c7cbef6cd62f6c
Alex <sasha2001-1@mail.ru>
2023-08-29 18:43:29 +0900

    Alex9HomeWorkAdded

---
+++ Alex/Tests/Null/Null/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr = null;
+
+            Console.WriteLine(myStr ??= "Нет строки");
+
+            Console.ReadKey();
+        }
+    }
+}
