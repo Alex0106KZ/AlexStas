e25e5c50643e178389b69d0cee69897835d8756e
StanislavAtangaraev <satangaraev@yandex.ru>
2023-08-30 19:54:50 +0600

    Commit delal otec

---
+++ StASS/If-Else/If-Else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Эта программа Дикого Санина");
+Console.WriteLine("Введите четное число");
+
+int b = 2;
+int a = int.Parse(Console.ReadLine());
+
+int result = a % b;
+    
+if (result == 0)
+{
+    Console.WriteLine("Вы Ввели четное число");
+}
+else
+{
+    Console.WriteLine("Вы ДОЛБОЕБ!");
+}
